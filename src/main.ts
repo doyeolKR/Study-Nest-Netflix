10baee6ed80324c999a9a640f1bfe89f1ff80208
doyeol <oktui2002@naver.com>
2025-03-02 03:35:15 +0900

    add global validation pipe with whitelist and forbidNonWhitelisted options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 것이 입력은 되지만, 필터링되어서 클라이언트에 반환되지는 않음
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 것은 입력이 되지 않고, 에러 발생
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
